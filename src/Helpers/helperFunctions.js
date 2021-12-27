29ccbef43953b09b8fe04355ff06fd9e087ba939
bart <bart_klumperman@Live.nl>
2021-12-27 15:59:31 +0100

    first test of helper funtion multiply

---
+++ src/Helpers/helperFunctions.js
@@ -0,0 +1,4 @@
+//Multiply function which will mutilpy 2 numbers and return the product 
+export const multiply = (num1, num2) => { 
+    return num1 * num2 
+}
---
+++ src/Helpers/helperFunctions.test.js
@@ -0,0 +1,5 @@
+import {multiply} from "./helperFunctions"
+
+test("multiply", () => { 
+    expect(multiply(2,20)).toBe(40); 
+}); 
