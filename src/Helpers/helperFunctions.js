8a084b9f7d82e1662b323a36c38030942843cb09
bart <bart_klumperman@Live.nl>
2022-01-09 13:13:44 +0100

    Updates

--- src/Helpers/helperFunctions.js
+++
@@ -1,9 +0,0 @@
-//Multiply function which will mutilpy 2 numbers and return the product 
-export const multiply = (num1, num2) => { 
-    return num1 * num2 
-}
-
-//Accepts a string and returns the string in full lowerCase 
-export const makeLowerCase = (string) => { 
-    return string.toLowerCase(); 
-}
--- src/lib/commerce.js
+++
@@ -1,4 +0,0 @@
-import Commerce from '@chec/commerce.js'; 
-
-export const commerce = new Commerce(process.env.REACT_APP_CHECK_PUBLIC_KEY, true); 
-
